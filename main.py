33eaf0ceee6a16d6c7e36d5a3b4361ad779b7e2a
geebus85 <garrisonwong@gmail.com>
2024-06-10 00:06:13 -0700

    removed redundant functions

--- main.py
+++ main.py
@@ -2,18 +2,11 @@ def main():
     book_path = "books/frankenstein.txt"
     book_text = get_text(book_path) 
     lowered_text = book_text.lower()
-    character_list = get_list_of_characters(lowered_text)
     wordcount = get_wordcount(book_text)
-    character_count = get_char_count(character_list)
-
+    character_count = get_char_count(lowered_text)
+    #print(f"{lowered_text}")
     print(f"{wordcount} words found in the document")
     print(f"{character_count}")
-
-def get_list_of_characters(text):
-    characters = []
-    for each in text:
-        characters.append(each)
-    return characters
 
 def get_char_count(text):
     counter = {}
